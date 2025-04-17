f9e786f6e272b1d5ba93b5a6d5cc03c4e093a290
bKlueber <bradyklueber@gmail.com>
2025-04-16 22:20:32 -0700

    finish sprint
    got the lab done, think its done to the specifications asked, definitely will never slack this hard on one again.

--- Lab2.java
+++ Lab2.java
@@ -73,9 +73,9 @@ class Planner {
             if (task.id == id) { //checks to see if current itteration matches user input id
             taskList.remove(task); //says if found remove that task
             break; //stops the loop
+            }
         }
     }
-
 
     // this method calls the task toString method
     // for each task that exists in the task list
@@ -113,8 +113,59 @@ class Lab2{
     @Param: Planner obj
     */
     static void addNewTask(Planner a){
-      
+
+        Scanner addTaskUser = new Scanner(System.in);
+;      
+        Day weekday = null;
+        
+        while(weekday == null) {
+            System.out.print("Enter day of the week: MON, TUES, WED, THUR, FRI, SAT, SUN");
+            String dayInput = addTaskUser.next().toUpperCase(); //autoconverts user input to upercase for error handling to be easier
        
+        if (dayInput.equals("MON") || dayInput.equals("TUES") || dayInput.equals("WED") ||
+        dayInput.equals("THURS") || dayInput.equals("FRI") || dayInput.equals("SAT") ||
+        dayInput.equals("SUN")) {
+
+            weekday = Day.valueOf(dayInput); 
+            } 
+            else {
+                System.out.println("Invalid input, please select a valid day of the week: ");
+            } //this converts the string into an enum, without this program would error 
+        }
+
+        int monthDay; //initializes monthDay
+        while(true){ //ensures the date selected makes logical sense. only uses average range of 1-31, meaning some dates could still have 'impossible' days
+            System.out.println("Enter the day of the month");
+            monthDay = addTaskUser.nextInt();
+            addTaskUser.nextLine();
+            if (monthDay > 0 && monthDay <= 31) {
+            break;
+            }
+            else {
+                 System.out.println("Error, invalid input. Date selected outside possible date range."); 
+            }
+        }
+
+        System.out.println("Enter task description: ");
+        String taskInfo = addTaskUser.nextLine();
+
+        String colorInput;
+        Color importance; 
+
+        while(true) { //this ensures only red, yellow or green are allowed as input
+            System.out.println("Please select importance level: RED, YELLOW, GREEN: ");
+            colorInput = addTaskUser.next().toUpperCase();
+        
+            if (colorInput.equals("RED") || colorInput.equals("YELLOW") || colorInput.equals("GREEN")) {
+                importance = Color.valueOf(colorInput); //only converts if matches
+                break;
+            }
+            else {
+            System.out.println("Invalid selection please try again");
+            }
+        }
+
+        a.addNewTask(weekday, monthDay, taskInfo, importance); //calls Planner.addNewTask and populates a new task with these arguments
 
     }
 
@@ -123,20 +174,51 @@ class Lab2{
         Scanner userInput = new Scanner(System.in);
         Planner myPlanner = new Planner();
 
+        while(true) { //this keeps the user returning to main menu
         System.out.println("Welcome to Your Digital Planner\nPlease select from the following menu options:");
         System.out.println("");
         System.out.println("1. Display all planner tasks");
         System.out.println("2. Add task to planner");
         System.out.println("3. Remove task from planner");
         System.out.println("4. Mark Planner task as complete");
+        System.out.println("5. Exit program");
         System.out.println("");
         System.out.println("Please make your selection:\n>>>");
         int userChoice = userInput.nextInt();
 
-        addNewTask(myPlanner);
-        
-        myPlanner.showAllTasks();
-
+         switch(userChoice) {
             
+            case 1:
+               myPlanner.showAllTasks();
+               break;
+
+            case 2:
+                addNewTask(myPlanner);
+                break;
+
+            case 3: 
+                System.out.println("Please eneter the Task ID you would like to remove: ");
+                int removeId = userInput.nextInt();
+                myPlanner.removeTaskById(removeId); //calls the remove task mathod and passes the user input as an arguement
+                break;
+
+            case 4: 
+                 System.out.println("Please eneter the Task ID you would like to mark as complete: ");
+                int completeId = userInput.nextInt();
+                myPlanner.markCompletion(completeId); //calls the remove task mathod and passes the user input as an arguement
+                break;
+
+            case 5:
+
+                userInput.close();
+                System.exit(0);
+                break;    
+
+            default:
+                System.out.println("Invalid selection please try again choosing from the menu options listed.");
+            
+        }
     }
+}
+
 }
